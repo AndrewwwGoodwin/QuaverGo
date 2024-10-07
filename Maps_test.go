128f369ed502e4c51b6f33e28d39c334469dacba
Andrew Goodwin <pgoodwi3@students.kennesaw.edu>
2024-10-07 07:25:34 -0400

    Maps.GetMods

--- Maps_test.go
+++ Maps_test.go
@@ -10,3 +10,12 @@ func TestMaps_ByID(t *testing.T) {
 	}
 	t.Log(returnedMap)
 }
+
+func TestMaps_GetMods(t *testing.T) {
+	client := Init()
+	returnedMapMods, err := client.Maps.GetMods(380)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	t.Log(returnedMapMods)
+}
