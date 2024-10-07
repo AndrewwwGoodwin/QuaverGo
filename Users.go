b406f1a5ba2713f4f032aaff0ba6f0830aa5cd84
Andrew Goodwin <pgoodwi3@students.kennesaw.edu>
2024-10-07 07:14:55 -0400

    move Map to Maps.go

--- Users.go
+++ Users.go
@@ -226,39 +226,6 @@ type Mapset struct {
 	Maps            []Map     `json:"maps"`
 }
 
-type Map struct {
-	Id                   int     `json:"id"`
-	MapsetId             int     `json:"mapset_id"`
-	Md5                  string  `json:"md5"`
-	AlternativeMd5       string  `json:"alternative_md5"`
-	CreatorId            int     `json:"creator_id"`
-	CreatorUsername      string  `json:"creator_username"`
-	GameMode             int     `json:"game_mode"`
-	RankedStatus         int     `json:"ranked_status"`
-	Artist               string  `json:"artist"`
-	Title                string  `json:"title"`
-	Source               string  `json:"source"`
-	Tags                 string  `json:"tags"`
-	Description          string  `json:"description"`
-	DifficultyName       string  `json:"difficulty_name"`
-	Length               int     `json:"length"`
-	Bpm                  float64 `json:"bpm"`
-	DifficultyRating     float64 `json:"difficulty_rating"`
-	CountHitobjectNormal int     `json:"count_hitobject_normal"`
-	CountHitobjectLong   int     `json:"count_hitobject_long"`
-	LongNotePercentage   float64 `json:"long_note_percentage"`
-	MaxCombo             int     `json:"max_combo"`
-	PlayCount            int     `json:"play_count"`
-	FailCount            int     `json:"fail_count"`
-	PlayAttempts         int     `json:"play_attempts"`
-	ModsPending          int     `json:"mods_pending"`
-	ModsAccepted         int     `json:"mods_accepted"`
-	ModsDenied           int     `json:"mods_denied"`
-	ModsIgnored          int     `json:"mods_ignored"`
-	OnlineOffset         int     `json:"online_offset"`
-	IsClanRanked         bool    `json:"is_clan_ranked"`
-}
-
 func (u Users) Playlists(id int) (*[]Playlists, error) {
 	responseData, err := u.APIClient.AttemptRequest(fmt.Sprintf("%s%s%d/playlists", u.APIClient.baseURL, u.EndpointExtension, id))
 	if err != nil {
