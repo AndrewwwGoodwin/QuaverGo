d3383f4e120c2e2c6a697f3c64b1169898832078
Andrew Goodwin <pgoodwi3@students.kennesaw.edu>
2024-10-09 09:50:40 -0400

    GetServerStats

--- ServerStats.go
+++ ServerStats.go
@@ -1,5 +1,7 @@
 package QuaverGo
 
+import "fmt"
+
 type ServerStats struct {
 	APIClient         *Client
 	EndpointExtension string
@@ -7,4 +9,20 @@ type ServerStats struct {
 
 func initServerStats(apiClient *Client) *ServerStats {
 	return &ServerStats{APIClient: apiClient, EndpointExtension: "/server/"}
+}
+
+func (s *ServerStats) GetServerStats() (ServerStatsData, error) {
+	var returnData ServerStatsData
+	err := fetchData(fmt.Sprintf("%s%sstats", s.APIClient.baseURL, s.EndpointExtension), &returnData)
+	if err != nil {
+		return ServerStatsData{}, err
+	}
+	return returnData, nil
+}
+
+type ServerStatsData struct {
+	OnlineUsers  int `json:"online_users"`
+	TotalMapsets int `json:"total_mapsets"`
+	TotalScores  int `json:"total_scores"`
+	TotalUsers   int `json:"total_users"`
 }
